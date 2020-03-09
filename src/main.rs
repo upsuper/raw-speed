0d6ba564786061dd6d8aab579800646efe9491ad
Xidorn Quan <me@upsuper.org>
2020-03-09 18:33:14 +1100

    Use structopt instead of clap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use clap::clap_app;
+use crate::protocol::Mode;
+use structopt::StructOpt;
 
 #[macro_use]
 mod protocol;
@@ -8,50 +9,36 @@ mod server;
 mod utils;
 
 const DEFAULT_ADDR: &str = "0.0.0.0";
-const DEFAULT_PORT: u16 = 7333;
+const DEFAULT_PORT: &str = "7333";
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "raw-speed", about = "Measure speed")]
+enum Command {
+    /// Run as server
+    Server {
+        /// Address to listen on
+        #[structopt(short, long, default_value = DEFAULT_ADDR)]
+        address: String,
+        /// Port to listen on
+        #[structopt(short, long, default_value = DEFAULT_PORT)]
+        port: u16,
+    },
+    /// Run as client
+    Client {
+        /// Testing mode, one of 'up', 'down', and 'both'
+        #[structopt(parse(try_from_str))]
+        mode: Mode,
+        /// Server address
+        server: String,
+        /// Server port
+        #[structopt(default_value = DEFAULT_PORT)]
+        port: u16,
+    },
+}
 
 fn main() {
-    let matches = clap_app!(rawspeed =>
-        (version: "0.1")
-        (author: "Xidorn Quan <me@upsuper.org>")
-        (about: "Measure speed")
-        (@subcommand server =>
-            (about: "Run as server")
-            (@arg address: -a --address +takes_value
-             "Address to listen on, 0.0.0.0 by default")
-            (@arg port: -p --port +takes_value
-             "Port to listen on, 7333 by default")
-        )
-        (@subcommand client =>
-            (about: "Run as client")
-            (@arg mode: +required
-             "Testing mode, one of 'up', 'down', and 'both'")
-            (@arg server: +required "Server address")
-            (@arg port: "Server port, 7333 by default")
-        )
-    )
-    .get_matches();
-
-    if let Some(server) = matches.subcommand_matches("server") {
-        let addr = server.value_of("address").unwrap_or(DEFAULT_ADDR);
-        let port = server
-            .value_of("port")
-            .map(|p| p.parse().expect("Invalid port number"))
-            .unwrap_or(DEFAULT_PORT);
-        server::run(addr, port);
-    } else if let Some(client) = matches.subcommand_matches("client") {
-        let mode = client
-            .value_of("mode")
-            .unwrap()
-            .parse()
-            .expect("Invalid mode");
-        let addr = client.value_of("server").unwrap();
-        let port = client
-            .value_of("port")
-            .map(|p| p.parse().expect("Invalid port number"))
-            .unwrap_or(DEFAULT_PORT);
-        client::run(addr, port, mode);
-    } else {
-        panic!("Must be run as server or client.");
+    match Command::from_args() {
+        Command::Server { address, port } => server::run(&address, port),
+        Command::Client { mode, server, port } => client::run(&server, port, mode),
     }
 }
